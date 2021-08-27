6e34d5a8efe494fdf4fc3d7d3e0b69804f83d82f
Ryan Kroon <rykroon.tech@gmail.com>
2021-08-27 18:11:09 -0400

    A router can be used as a WSGI App.

--- werklette/routing.py
+++ werklette/routing.py
@@ -1,4 +1,13 @@
-from werkzeug.routing import Rule
+from werkzeug.routing import Map, Rule
+from werklette.requests import Request
+
+
+class Router(Map):
+    def __call__(self, request):
+        adapter = self.bind_to_environ(request.environ)
+        func, kwargs = adapter.match(path_info=request.path, method=request.method)
+        request._path_params = kwargs
+        return func(request)
 
 
 class Route(Rule):
