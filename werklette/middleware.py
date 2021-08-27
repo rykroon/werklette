289e8975c68242376f2a9a38ef39ee30db1ecd81
Ryan Kroon <rykroon.tech@gmail.com>
2021-08-27 18:11:34 -0400

    Added Exception Middleware.

--- werklette/middleware.py
+++ werklette/middleware.py
@@ -1,3 +1,4 @@
+from werkzeug.exceptions import HTTPException
 
 
 class Middleware:
@@ -8,3 +9,17 @@ class Middleware:
 
     def __call__(self, request):
         ...
+
+
+class ExceptionMiddleware:
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, request):
+        try:
+            return self.app(request)
+        
+        except HTTPException as e:
+            return e
+
