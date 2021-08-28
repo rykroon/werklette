ef92b87f0de2321bf7b0e7cbab686dd67b65ec28
Ryan Kroon <rykroon.tech@gmail.com>
2021-08-28 16:06:45 -0400

    Added set_path_params() method.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from werklette import Werklette
 from werklette.responses import JsonResponse
+from werklette.routing import Router, Route
 
 
 app = Werklette()
@@ -8,4 +9,10 @@ app = Werklette()
 @app.get('/')
 def homepage(request):
     return JsonResponse({'Hello': 'World'})
+
+
+# A Router can be a stand-alone WSGI App.
+router = Router([
+    Route('/', endpoint=homepage, methods=['GET'])
+])
 
--- werklette/routing.py
+++ werklette/routing.py
@@ -1,14 +1,15 @@
-from werkzeug.datastructures import ImmutableDict
 from werkzeug.routing import Map, Rule
+from werklette.requests import Request
 
 
 class Router(Map):
 
-    def __call__(self, request):
+    def __call__(self, environ, start_response):
+        request = Request(environ)
         adapter = self.bind_to_environ(request.environ)
         func, kwargs = adapter.match(path_info=request.path, method=request.method)
-        request._path_params = ImmutableDict(kwargs)
-        return func(request)
+        request.set_path_params(kwargs)
+        return func(request)(environ, start_response)
 
 
 class Route(Rule):
