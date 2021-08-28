127cef0c072dea2f18c702dd83ef225d031fc805
Ryan Kroon <rykroon.tech@gmail.com>
2021-08-28 15:04:03 -0400

    Updated path_params to be an ImmutableDict.

--- werklette/routing.py
+++ werklette/routing.py
@@ -1,12 +1,13 @@
+from werkzeug.datastructures import ImmutableDict
 from werkzeug.routing import Map, Rule
-from werklette.requests import Request
 
 
 class Router(Map):
+
     def __call__(self, request):
         adapter = self.bind_to_environ(request.environ)
         func, kwargs = adapter.match(path_info=request.path, method=request.method)
-        request._path_params = kwargs
+        request._path_params = ImmutableDict(kwargs)
         return func(request)
 
 
