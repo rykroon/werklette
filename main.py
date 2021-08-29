324094f15418d1cea3bc6119d3d7e1648fb7a9fb
Ryan Kroon <rykroon.tech@gmail.com>
2021-08-29 13:43:28 -0400

    Added exception_handler example.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from werkzeug.exceptions import HTTPException
 from werklette import Werklette
 from werklette.responses import JsonResponse
 from werklette.routing import Router, Route
 
 
-app = Werklette(debug=True)
+def http_exception_handler(req, exc):
+    return JsonResponse({
+        'error': exc.name,
+        'error_description': exc.description
+    }, status=exc.code)
+
+
+exception_handlers = {
+    HTTPException: http_exception_handler
+}
+
+app = Werklette(debug=True, exception_handlers=exception_handlers)
 
 
 @app.get('/')
