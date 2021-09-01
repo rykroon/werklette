d283d1d1c3476c0afd1bc0ca22073bbba82f17d4
Ryan Kroon <rykroon.tech@gmail.com>
2021-09-01 19:58:23 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from datetime import date
+from setuptools import setup, find_packages
+
+# python setup.py bdist_wheel
+
+version = date.today().isoformat().replace('-', '.')
+
+setup(
+    name='werklette',
+    version=version,
+    author='Ryan Kroon',
+    author_email='rykroon.tech@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
