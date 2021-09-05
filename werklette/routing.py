fd8b27a684a69ada6c052e863dc7e2ce6616b74c
Ryan Kroon <rykroon.tech@gmail.com>
2021-09-04 22:15:52 -0400

    Fixed bug when using a router as an app.

--- werklette/routing.py
+++ werklette/routing.py
@@ -5,11 +5,17 @@ from werklette.requests import Request
 class Router(Map):
 
     def __call__(self, environ, start_response):
+        # If there isn't already a Request object that was made
+        # then the router is the app.
+        router_as_app = 'werkzeug.request' not in environ
         request = environ.get('werkzeug.request') or Request(environ)
         adapter = self.bind_to_environ(request.environ)
         endpoint, kwargs = adapter.match(path_info=request.path, method=request.method)
         request._set_path_params(kwargs)
-        return endpoint(request)
+        response = endpoint(request)
+        if router_as_app:
+            return response(environ, start_response)
+        return response
 
 
 class Route(Rule):
